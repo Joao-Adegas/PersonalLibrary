5b025ef389f475becd0ef4f1971dce55395e14c5
Joao-Adegas <pedrojoao6378@gmail.com>
2025-08-14 00:54:29 -0300

    add a switc case in main

--- PersonalLibrary/src/Library.java
+++ PersonalLibrary/src/Library.java
@@ -1,9 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Library {
     private List<Book> listBooksDisponiveis = new ArrayList<>();
     private List<Book> listBooksIndisponiveis = new ArrayList<>();
+
+    Scanner getData = new Scanner(System.in);
     /*
      * 1 - Cadastrar Livros
      *   - O usuário deverá informar o título, autor e ISBN do livro. O livro começa como disponível e ( não emprestado );
@@ -20,23 +23,38 @@ public class Library {
 
     public void showMenu(){
         System.out.println("Digite oque deseja fazer (1,2,3)\n" +
-                "1. Adicionar um livro\n2. Emprestar Livro\n3. Devolver Livro\n4. Listar Livros");
+                "1. Adicionar um livro\n2. Emprestar Livro\n3. Devolver Livro\n4. Listar Livros\n5. Sair\n\n");
 
     }
 
     // Cadastrar Livro
-    public void addBook(Book b){
+    public void addBook(){
+            Book b = new Book();
+
+            System.out.println("Digite o titulo do livro:\n");
+            String titleBook = getData.nextLine();
+            System.out.println("Digite o ISBN do livro:\n");
+            String isbn = getData.nextLine();
+            System.out.println("Digite o autor do livro:\n");
+            String author = getData.nextLine();
+
+            b.setStatus(true);
+            b.setTitle(titleBook);
+            b.setIsbn(isbn);
+            b.setAuthor(author);
+
             listBooksDisponiveis.add(b);
-            System.out.println("Livro " + b.getTitle() + " Adicionado com sucesso!!");
+
+            System.out.println("Livro " + b.getTitle() + " Adicionado com sucesso!!\n");
     }
 
     // Emprestar Livro
     public void lendBool(Book b){
         if(b.isStatus() == false){
-            System.out.println("O livro " + b.getTitle() + " não esta disponível para ser emprestado");
-        }
+            System.out.println("O livro " + b.getTitle() + " não esta disponível para ser emprestado\n");
+    }
         else if(b.isStatus() == true){
-            System.out.println("Livro " + b.getTitle() + " emprestado com sucesso!!");
+            System.out.println("Livro " + b.getTitle() + " emprestado com sucesso!!\n");
             b.setStatus(false);
             listBooksDisponiveis.remove(b);
             listBooksIndisponiveis.add(b);
@@ -46,16 +64,16 @@ public class Library {
     // Devolvere Livro
     public void returnBook(Book b){
         if(b.isStatus() == false){
-            System.out.println("Livro devolvido com sucesso!");
+            System.out.println("Livro devolvido com sucesso!\n");
             b.setStatus(true);
         }
         else if(b.isStatus() == true){
-            System.out.println("Não é possível devolver o livro por conta que ele ja não estava emprestado!!");
+            System.out.println("Não é possível devolver o livro por conta que ele ja não estava emprestado!!\n");
         }
     }
 
     // Listar Livros
     public String listBooks(){
-        return "Livros DIsponiveis:\n" + listBooksDisponiveis;
+        return "Livros Disponiveis:\n" + listBooksDisponiveis;
     }
 }
--- PersonalLibrary/src/LibraryApp.java
+++ PersonalLibrary/src/LibraryApp.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class LibraryApp {
@@ -7,16 +8,32 @@ public class LibraryApp {
         Library library = new Library();
         Scanner getData = new Scanner(System.in);
 
-        System.out.println("=============== Bem-vindo(a) Biblioteca Pessoal ===================");
-        library.showMenu();
-        /*
-        * try {
-            int option = getData.nextInt();
-        }catch (Exception){
+        System.out.println("=============== Bem-vindo(a) Biblioteca Pessoal ===================\n");
 
-        }
 
-        * */
+        int option = 0;
+        do{
+            library.showMenu();
+            try {
+                option = getData.nextInt();
+            }catch (InputMismatchException e){
+                System.out.println("Erro: Você deve digitar um numero de 1 a 5!!\n");
+                break;
+            }
+
+            switch (option){
+                case 1:
+                    System.out.printf("Voce digitou " + option + ":\n");
+                    library.addBook();
+                break;
+            }
+
+        }while(option != 5);
+
+        System.out.println("============ Até mais =============");
+
+
+
 
 
     }
